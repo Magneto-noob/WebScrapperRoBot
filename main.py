d44110ee32d184d953f50607458e07bd7074f1b9
Magneto-noob <87361323+Magneto-noob@users.noreply.github.com>
2021-10-14 22:58:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ async def scrapping(bot, message):
         url = str(message.text)
         request = requests.get(url)
       #  await txt.edit(text=f"Getting Raw Data from {url}", disable_web_page_preview=True)
-      #  file_write = open(f'RawData-{message.chat.username}.txt', 'a+')
+      #  file_write = open(f'RawData-{message.chat.use.txt', 'a+')
       #  file_write.write(f"{request.content}")  # Writing Raw Content to Txt file
       #  file_write.close()
       #  await message.reply_document(f"RawData-{message.chat.username}.txt", caption="©@BugHunterBots", quote=True)
@@ -47,12 +47,12 @@ async def scrapping(bot, message):
    # try:
         txt = await message.reply_text(text=f"Getting HTML Code From {url}", disable_web_page_preview=True, quote=True)
         soup = BeautifulSoup(request.content, 'html5lib')  # Extracting Html code in Tree Format
-        file_write = open(f'Link_No-{message.message_id}', 'a+')
+        file_write = open(f'Web-Srapper-Robot', 'a+')
         soup.data = soup.prettify()  # parsing HTML
         file_write.write(f"{soup.data}")  # writing data to txt
         file_write.close()
-        await message.reply_document(f"Link_No-{message.message_id}.txt", caption="Cᴏᴅᴇ Gᴇɴᴇʀᴀᴛᴇᴅ Bʏ @WebScraperRobot", quote=True)
-        os.remove(f"Link_No-{message.message_id}.txt")
+        await message.reply_document(f"Link-No-{message.message_id}.txt", caption="Cᴏᴅᴇ Gᴇɴᴇʀᴀᴛᴇᴅ Bʏ @WebScraperRobot", quote=True)
+        os.remove(f"Link-No-{message.message_id}.txt")
         await txt.delete()
     except Exception as error:
         await message.reply_text(text=f"{error}", disable_web_page_preview=True, quote=True)

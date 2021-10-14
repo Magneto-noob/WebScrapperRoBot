0ef40471d0ac0301efaf5b603695def07e8d9298
Magneto-noob <87361323+Magneto-noob@users.noreply.github.com>
2021-10-14 22:37:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,12 +47,12 @@ async def scrapping(bot, message):
    # try:
         txt = await message.reply_text(text=f"Getting HTML Code From {url}", disable_web_page_preview=True, quote=True)
         soup = BeautifulSoup(request.content, 'html5lib')  # Extracting Html code in Tree Format
-        file_write = open(f'{get_msg.message_id}', 'a+')
+        file_write = open(f'{message.id}', 'a+')
         soup.data = soup.prettify()  # parsing HTML
         file_write.write(f"{soup.data}")  # writing data to txt
         file_write.close()
-        await message.reply_document(f"{get_msg.message_id}.txt", caption="Cᴏᴅᴇ Gᴇɴᴇʀᴀᴛᴇᴅ Bʏ @WebScraperRobot", quote=True)
-        os.remove(f"{get_msg.message_id}.txt")
+        await message.reply_document(f"{message.id}.txt", caption="Cᴏᴅᴇ Gᴇɴᴇʀᴀᴛᴇᴅ Bʏ @WebScraperRobot", quote=True)
+        os.remove(f"{message.id}.txt")
         await txt.delete()
     except Exception as error:
         await message.reply_text(text=f"{error}", disable_web_page_preview=True, quote=True)

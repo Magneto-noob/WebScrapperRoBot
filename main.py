15e02840af3c1c95a0d48c4f1eb78afd2bb91f4d
Magneto-noob <87361323+Magneto-noob@users.noreply.github.com>
2021-12-29 23:04:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,16 +58,12 @@ async def scrapping(bot, message):
       # file_write.close()
       #  caption = f"`Title :- {titles}`\n\n**Video URL :-** {links}"
       # time.sleep(3)
+        await message.reply_text(text=f"`Title :- {titles}`\n\n**Video URL :-** {links}", quote=True)
+      # os.remove(f"{titles}.txt")
         await txt.delete()
     except Exception as error:
         await message.reply_text(text=f"{error}", disable_web_page_preview=True, quote=True)
         await txt.delete()
-        await message.reply_text(text=f"`Title :- {titles}`\n\n**Video URL :-** {links}", quote=True)
-      # os.remove(f"{titles}.txt")
-      #  await txt.delete()
-  #  except Exception as error:
-       # await message.reply_text(text=f"{error}", disable_web_page_preview=True, quote=True)
-       # await txt.delete()
         return
 
 # Use soup.find_all('tag_name') to Extract Specific Tag Details

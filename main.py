e35c1e3ec7d3ba2aac0dbd56a296d6e7f7b5ad75
Magneto-noob <87361323+Magneto-noob@users.noreply.github.com>
2021-12-29 16:40:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -60,10 +60,10 @@ async def scrapping(bot, message):
       # time.sleep(3)
         await message.reply_document(f"{titles}.txt", caption=caption, quote=True)
         os.remove(f"{titles}.txt")
-        await text.delete()
+        await txt.delete()
     except Exception as error:
         await message.reply_text(text=f"{error}", disable_web_page_preview=True, quote=True)
-        await text.delete()
+        await txt.delete()
         return
 
 # Use soup.find_all('tag_name') to Extract Specific Tag Details

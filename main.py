5ef5084fedc2d439af8b7043477bde1fbd606c0e
Magneto-noob <87361323+Magneto-noob@users.noreply.github.com>
2022-01-11 14:53:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ async def scrapping(bot, message):
       # file_write.close()
       #  caption = f"{links} * {titles}"
       # time.sleep(3)
-        await message.reply_text(text=f"{links} * {titles}", quote=True)
+        await message.reply_text(text=f"{links} * Title :- {titles}", quote=True)
       # os.remove(f"{titles}.txt")
         await txt.delete()
     except Exception as error:
